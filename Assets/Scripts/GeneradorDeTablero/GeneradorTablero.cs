d15d251554dbeb292c3251f46784c590eaf17eca
SergioVelasquez-D <109398726+SergioVelasquez-D@users.noreply.github.com>
2023-04-28 02:13:45 -0500

    First Commit Branch Gameplay
    Script de generador de tablero
    Prefab Casilla
    Carpetas para trabajo (Scripts, Prefabs, Generador)

---
+++ Assets/Scripts/GeneradorDeTablero/GeneradorTablero.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GeneradorTablero : MonoBehaviour
+{
+    //Dimensiones del tablero
+    static int ancho = 10;
+    static int largo = 10;
+
+    //Posicion de la casilla
+    static float casillaOffSetX = 2;
+    static float casillaOffSetZ = 2;
+    
+    //Coloca una pestaña en el menu para crear desde el tablero desde Unity
+    [MenuItem("Custom/CrearTablero")]
+
+    //Metodo para buscar objetos en jerarquia e instanciar casillas
+    static void CrearTablero()
+    {
+        GameObject casillaPrefab = GameObject.FindGameObjectWithTag("CasillaPrefab");
+        GameObject tablero = GameObject.FindGameObjectWithTag("Tablero");
+    
+        // Crear casillas desde Prefabs, colocarles nombre de "lugar en el tablero"
+        for (int i = 0; i < ancho; i++)
+        {
+            for (int j = 0; j < ancho; j++)
+            {
+                GameObject goCasilla = Instantiate(casillaPrefab, tablero.transform);
+                goCasilla.name = $"{i}, {j}";
+
+                goCasilla.transform.position = new Vector3(i*casillaOffSetX, 0.01f, j*casillaOffSetZ);
+            }  
+
+        }
+    }
+}
